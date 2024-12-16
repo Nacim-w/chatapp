dff8ef138f30c6eb42364b4c6f9a5fb988e37f95
Nacim-w <89641819+Nacim-w@users.noreply.github.com>
2024-12-16 18:26:30 +0100

    Update index.js

--- config/index.js
+++ config/index.js
@@ -2,14 +2,14 @@ import app from "firebase/compat/app";
 import "firebase/compat/auth";
 import "firebase/compat/database";
 const firebaseConfig = {
-  apiKey: "AIzaSyDiq2xbFCevTNtMFw9c6ZWWoFO3AOnNRP0",
-  authDomain: "whatsappclone-bf545.firebaseapp.com",
-  databaseURL: "https://whatsappclone-bf545-default-rtdb.firebaseio.com",
-  projectId: "whatsappclone-bf545",
-  storageBucket: "whatsappclone-bf545.firebasestorage.app",
-  messagingSenderId: "940645215764",
-  appId: "1:940645215764:web:cb9e6b3972286470e18101",
-  measurementId: "G-TP2BLH4RM0"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 const firebase = app.initializeApp(firebaseConfig);
@@ -19,7 +19,7 @@ export default firebase;
 
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://ycnunrrfuxkxfzggaqwf.supabase.co'
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InljbnVucnJmdXhreGZ6Z2dhcXdmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzI3MDY4ODEsImV4cCI6MjA0ODI4Mjg4MX0.N_dOyawE008QgSU0-Yd_YWW0iRwMj3bU90tNq4N5lQ0"
+const supabaseUrl = ''
+const supabaseKey = ""
 const supabase = createClient(supabaseUrl, supabaseKey)
 export {supabase};
